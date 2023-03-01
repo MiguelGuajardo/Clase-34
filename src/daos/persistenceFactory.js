b4d04f02dbdecbf7710c0edbdd25b58d7706322b
Miguel Guuajardo <miguelguajardo1468@hotmail.com>
2023-03-01 16:31:10 -0300

    persistenceFactory

--- src/daos/persistenceFactory.js
+++ src/daos/persistenceFactory.js
@@ -7,10 +7,8 @@ class PersistenceFactory{
     static getPersistence = async () =>{
         switch(config.APP.persistence){
             case "MEMORY":
-                /* let {default: ProductDaoMemory} = await require("./productDaoMemory") */
                 return new ProductDaoMemory()
             case "FILE":
-                /* let {default: ProductDaoFile} = await require("./productDaoFile") */
                 return new ProductDaoFile()
             case "MONGO":
                 return new ProductDaoMongo()

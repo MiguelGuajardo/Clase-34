89aaed6ff5e3002b0a9285a6ed6fb4e5ae90e7a1
Miguel Guuajardo <miguelguajardo1468@hotmail.com>
2023-03-01 15:43:21 -0300

    db

--- src/database.js
+++ src/database.js
@@ -1,8 +1,7 @@
-const { config } = require("./config/index")
 const mongoose = require("mongoose")
 const Logger = require("./utils/logger")
 const logger = new Logger()
 mongoose.set('strictQuery', true);
-mongoose.connect(config.DATABASE.mongo.mongoUrl, {useNewUrlParser: true})
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true})
 .then(db => logger.info("Base de Datos mongoDB conectada"))
 .catch(err => logger.error(err))
